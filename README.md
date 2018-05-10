117e3db88b104d4951ba9ba6501d9f2a9db41c80
energy591 <839205143@qq.com>
2018-05-11 02:53:44 +0800

    add react-redux

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 - ReactDOM.render方法简易实现
 - redux createstore简易版
 - redux combineReducers方法
-- 高阶组件的实现
+- 高阶组件的实现（达到组件复用的目的）
+- react-redux简易实现（基于react16.3 的context来实现的）
---
+++ react-redux.js
@@ -0,0 +1,49 @@
+//{Provider,connect}
+import React from 'react'
+let {Provider:Pro,Consumer}=React.createContext();
+//Provider 中有一个属性 store
+class Provider extends React.Component {
+  render() {
+    return <Pro value={this.props.store}>
+      {this.props.children}
+    </Pro>
+  }
+}
+
+let connect = (mapStateToProps, mapDispatchToProps) => (Component) => {
+  return class Proxy extends React.Component {
+    render() {
+      return (
+        <Consumer>
+          {(store)=>{
+            console.log(store);
+            // let state=mapStateToProps(store.getState())
+            // let actions=mapDispatchToProps(store.dispatch)
+            // //状态变化后 需要更新视图 调用this.setState()
+            //
+            // return <Component {...state} {...actions}/>
+            class High extends React.Component{
+              state=mapStateToProps(store.getState())
+              componentDidMount(){
+                console.log(this.state);
+                this.unsub=store.subscribe(()=>{
+                  this.setState(mapStateToProps(store.getState()))
+                })
+              }
+              componentWillUnmount(){
+                this.unsub();
+              }
+              render(){
+                let actions=mapDispatchToProps(store.dispatch)
+                return <Component {...this.state} {...actions}/>
+              }
+            }
+            return <High/>
+          }}
+
+        </Consumer>
+      )
+    }
+  }
+}
+export {Provider,connect}
